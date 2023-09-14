c8dac98ec10cd39dcb03cebb2f55656b839d8aa4
dolsen0 <derek.olsen@gmail.com>
2023-09-14 19:05:49 -0400

    added background image to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 "use client"
 
-
-import Image from 'next/image'
-import styles from './page.module.css'
 import GetLocation from './components/GetLocation.jsx'
 
 export default function Home() {
   return (
     <>
+    <section className='app__background'>
+
       <h1>Weather</h1>
       
       <GetLocation />  
+    </section>
     </>
   )
 }
